34758feb8059180c6b0bc471ea024b03076130b2
Github <[Github>
2023-08-29 09:02:33 +0900

    search_result.java

---
+++ app/src/main/java/com/example/cat_airplane/tip_result.java
@@ -0,0 +1,14 @@
+package com.example.cat_airplane;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tip_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_result);
+    }
+}
