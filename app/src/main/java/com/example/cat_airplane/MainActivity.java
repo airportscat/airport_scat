65387d823701c31eb5d5f8347c41d38e87b0fc72
jiyeon11 <leejiyeon415@gmail.com>
2023-08-31 18:50:47 +0900

    [feat] start 버튼 눌렀을 때 선택지 화면으로 이동

--- app/src/main/java/com/example/cat_airplane/MainActivity.java
+++ app/src/main/java/com/example/cat_airplane/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.cat_airplane;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btnstart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnstart = findViewById(R.id.btnstart);
+
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //choose로 화면전환
+                Intent intent = new Intent(MainActivity.this,choose.class);
+                startActivity(intent);
+            }
+        });
     }
 }
