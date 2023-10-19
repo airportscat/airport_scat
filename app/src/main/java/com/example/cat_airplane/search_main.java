495f1f073e4186af02d35ae960194379b3b611a3
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-10-19 20:04:51 +0900

    :sparkles: 아이디별로 버튼클릭 후 파일 경로가 보임

--- app/src/main/java/com/example/cat_airplane/search_main.java
+++ app/src/main/java/com/example/cat_airplane/search_main.java
@@ -14,41 +14,73 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class search_main extends AppCompatActivity {
-    private ImageButton[] btn_sample = new ImageButton[8];
-    private Integer[] btn_id = {R.id.drink, R.id.charger, R.id.food, R.id.cane, R.id.medicine, R.id.lighter, R.id.ashes, R.id.spray};
+    private ImageButton btn_spary, btn_ashes, btn_drink, btn_cane, btn_charger, btn_medicine, btn_food, btn_lighter;
     private int i;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_main);
 
+        btn_spary = findViewById(R.id.spray);
+        btn_ashes = findViewById(R.id.ashes);
+        btn_drink = findViewById(R.id.drink);
+        btn_cane = findViewById(R.id.cane);
+        btn_charger = findViewById(R.id.charger);
+        btn_medicine = findViewById(R.id.medicine);
+        btn_food = findViewById(R.id.food);
+        btn_lighter = findViewById(R.id.lighter);
+
         //타이틀 바 없애기
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        //버튼 객체 등록
-        for (i = 0; i<btn_id.length; i++){
-            btn_sample[i] = (ImageButton) findViewById(btn_id[i]);
-        }
-
         //화면 넘기기위한 intent 객체 생성
-        Intent intent = new Intent(search_main.this, search_result.class);
+        intent = new Intent(search_main.this, search_result.class);
 
-        //버튼 이벤트 넣기
-        for (i = 0; i<btn_sample.length; i++){
-            btn_sample[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    switch (i){
-                        case 1:
-                            intent.putExtra("baggage", R.raw.cane); break;
-                    }
-                    startActivity(intent);
+        //버튼 객체 등록
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.spray:
+                        //스프레이
+                        intent.putExtra("baggage", R.raw.spray);
+                        break;
+                    case R.id.ashes:
+                        //유골
+                        intent.putExtra("baggage", R.raw.ashes); break;
+                    case R.id.drink:
+                        //음료수
+                        intent.putExtra("baggage", R.raw.drink); break;
+                    case R.id.cane:
+                        //지팡이
+                        intent.putExtra("baggage", R.raw.cane); break;
+                    case R.id.charger:
+                        //충전기
+                        intent.putExtra("baggage", R.raw.charger); break;
+                    case R.id.medicine:
+                        //약
+                        intent.putExtra("baggage", R.raw.medicine); break;
+                    case R.id.food:
+                        //음식
+                        intent.putExtra("baggage", R.raw.food); break;
+                    case R.id.lighter:
+                        //라이터
+                        intent.putExtra("baggage", R.raw.lighter); break;
                 }
-            });
+                startActivity(intent);
+            }
 
-        }
-
+        };
+        btn_spary.setOnClickListener(clickListener);
+        btn_ashes.setOnClickListener(clickListener);
+        btn_drink.setOnClickListener(clickListener);
+        btn_cane.setOnClickListener(clickListener);
+        btn_charger.setOnClickListener(clickListener);
+        btn_medicine.setOnClickListener(clickListener);
+        btn_food.setOnClickListener(clickListener);
+        btn_lighter.setOnClickListener(clickListener);
     }
 }
--- app/src/main/java/com/example/cat_airplane/search_result.java
+++ app/src/main/java/com/example/cat_airplane/search_result.java
@@ -19,11 +19,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class search_result extends AppCompatActivity {
     TextView search_result;
     Intent intent;
-    String baggage;
+    int baggage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +38,34 @@ public class search_result extends AppCompatActivity {
         actionBar.hide();
 
         intent = getIntent();
-        baggage = intent.getExtras().getString("baggage"); //메인에서 받아온 키 값으로 데이터 불러오기
+        //메인에서 받아온 키 값으로 데이터 불러오기
+        baggage = intent.getExtras().getInt("baggage");
 
-        try{
-            InputStream input = getResources().openRawResource(baggage);  //아이디좀받아와주라...
+        try {
+            // 데이터 파일 불러오기
+//            InputStream inputStream = getAssets().open(baggage+".txt");
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//            StringBuilder stringBuilder = new StringBuilder();
+//            String line;
+//
+//            //데이터 읽기
+//            while ((line = reader.readLine()) != null) {
+//                stringBuilder.append(line).append("\n");
+//            }
+//            reader.close();
+            InputStream input = getResources().openRawResource(baggage);
             byte[] txt = new byte[input.available()];
             input.read(txt);
-            String text = new String(txt);
-            search_result.setText(text);
             input.close();
 
             Toast.makeText(getApplicationContext(), "불러오기 성공", Toast.LENGTH_SHORT).show();
+
         } catch (IOException e) {
             e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "파일 없음", Toast.LENGTH_SHORT).show();
         }
+
+        //화면에 텍스트 데이터 보이기
+        search_result.setText(baggage);
     }
 }
