d6bd32bd93f4791411f316b3a84d622c8d9d7a0a
jiyeon11 <leejiyeon415@gmail.com>
2023-10-22 22:54:25 +0900

    🚧 try merge

---
+++ app/src/main/java/com/example/cat_airplane/TipDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.cat_airplane;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_detail);
+    }
+}
